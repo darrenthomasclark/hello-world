fd12fc8163f92ccaa92eab185477382759150389
Darren Thomas Clark <darren.clark@live.com>
2016-08-08 23:25:42 -0400

    initial upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Body from './Body'
+import './App.css'
+
+class App extends Component {
+
+  render () {
+    return (
+      <div className='App'>
+        <div className='App-header'>
+          <h2>all things darren.</h2>
+          <h3>Portfolio Music Blog</h3>
+        </div>
+        <Body />
+      </div>
+    )
+  }
+}
+export default App
---
+++ src/Body.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import './Body.css'
+
+class Body extends Component {
+
+  render () {
+    return (
+    <div className= 'bodycontainer'>
+      <div className= 'topline'>
+        <div className='h1body'>This is a pretty long blog post title.</div>
+        <div className='h2date'>MMMM, DD, YYYY</div>
+      </div>
+      <div className='bodycopy'>
+      <p>by <a href='#'><b>Darren Clark</b></a> - Web Designer / Developer</p>
+      <p>There is one story in particularly that I'd like to leave you with today - a story I told when I had the great honor of speaking on Dr. And I will not settle for an America where some kids don't have that chance. I will rebuild our military to meet future conflicts. The reality of gun ownership may be different for hunters in rural Ohio than for those plagued by gang-violence in Cleveland, but don't tell me we can't uphold the Second Amendment while keeping AK-47s out of the hands of criminals. And that's to be expected.</p>
+
+      <p>We are the party of Roosevelt. That is the world we seek. That's why pastors, friends of mine like Rick Warren and T.D. Jakes and organizations like World Vision and Catholic Charities are wielding their enormous influence to confront poverty, HIV/AIDS, and the genocide in Darfur. Most working- and middle-class white Americans don't feel that they have been particularly privileged by their race. Let us be our brother's keeper, Scripture tells us. For over two decades, he's subscribed to that old, discredited <a href='#'>Republican philosophy</a> - give more and more to those with the most and hope that prosperity trickles down to everyone else. I've seen it in Washington, when we worked across party lines to open up government and hold lobbyists more accountable, to give better care for our veterans and keep nuclear weapons out of terrorist hands. Trade can bring new wealth and opportunities, but also huge disruptions and changing communities.</p>
+
+      <p>That we can participate in the political process without fear of retribution, and that our votes will be counted at least, most of the time. And all across the country, communities of faith are sponsoring day care programs, building senior centers, and in so many other ways, taking part in the project of American renewal. These are the Americans that I know. <a href='#'>Our problems</a> must be dealt with through partnership; progress must be shared. Now is the time for Palestinians to focus on what they can build. But it is clear to all concerned that when it comes to nuclear weapons, we have reached a decisive point.</p>
+
+      <p>A belief that there are better days ahead. We need a President who can face the threats of the future, not keep grasping at the ideas of the past.</p>
+      </div>
+    </div>
+    )
+  }
+}
+export default Body
